46e9c6b448e7c44c148dc96cf6f34d6f5cf4600d
ssneller <ssneller@Steves-MacBook-Pro.local>
2014-07-23 10:15:41 -0400

    create list app

---
+++ ListApp/ListApp/MFCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MFCAppDelegate.h
+//  ListApp
+//
+//  Created by Steve Sneller on 7/23/14.
+//  Copyright (c) 2014 Steve Sneller. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MFCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
